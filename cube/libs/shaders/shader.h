191490a49e81cffb49e9349cee722965d51e0979
derpen <rizqyassabil@hotmail.com>
2024-10-12 14:09:04 +0700

    Cleaning up old

--- cube/libs/shaders/shader.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <string>
-
-unsigned int createShaderProgram(const std::string &vertexPath, const std::string &fragmentPath);
-std::string readShaderSource(const std::string &filepath);
-unsigned int compileShader(unsigned int type, const std::string &source);
--- cube/libs/textures/texture_loader.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-unsigned int loadTexture(const char* path);
