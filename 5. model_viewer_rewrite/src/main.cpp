f7a1cfdf7aa34f3a29f7b5197d5500e3260e813c
derpen <rizqyassabil@hotmail.com>
2025-08-26 11:30:22 +0700

    I get WinMain now. Visual Studio is being a cunt for removing all my configurations.

--- 5. model_viewer_rewrite/src/main.cpp
+++ 5. model_viewer_rewrite/src/main.cpp
@@ -1,20 +1,23 @@
-#include "iostream"
+#include <iostream>
 #include "opengl/loop.hpp"
-
 
 #ifdef _WIN32
     #include <windows.h>
 
-    int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    #ifdef _DEBUG
+        int main(int argc, char* argv[]) {
+    #else
+        int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    #endif
 #else
-    int main(int argc, char* argv[]) {
+        int main(int argc, char* argv[]) {
 #endif
-		int success = gl_loop::init_gl(800.0f, 600.0f, "dEngine");
-		if(success != 0){
-			std::cout << "An Error Occured\n";
-			return -1;
-		}
 
-		return 0;
+        int success = gl_loop::init_gl(800.0f, 600.0f, "dEngine");
+        if (success != 0) {
+            std::cout << "An Error Occurred\n";
+            return -1;
+        }
 
+        return 0;
     }
